49a350e541de2ca241098bd81564ad8aedb15bc6
Fanny <fannymuzizi@gmail.com>
2023-02-16 11:31:11 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Aprogram that prints a line using printf function
  * Return: 0 (Success)
@@ -7,5 +6,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,with proper grammar,\n");
-return (0);
+			return (0);
 }	
