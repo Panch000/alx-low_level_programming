cd2dd51b8fb2f618cf0033a03ad8f36a07ac9117
Fanny <fannymuzizi@gmail.com>
2023-02-16 11:11:59 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Aprogram that prints a line using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,with proper grammar,\n");
+	return (0);
+}	
